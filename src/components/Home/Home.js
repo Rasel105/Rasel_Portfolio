9d031d6f7228a534f4c84fe37ed7b22bab992ec6
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-06-09 19:35:43 +0600

    download option added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,13 +18,15 @@ const Home = () => {
                     designing) exceptional digital experiences. Currently, I'm focused on
                     building responsive full-stack web applications.
                 </p>
-                <div>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                        View Work
+                <div className='flex justify-start'>
+                    <a className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'
+                        href='https://drive.google.com/file/d/19QeOecjpDwHMdwrzBosWT7_SdzfEFVbo/view?usp=sharing' target="_blank" rel="noopener noreferrer"
+                    >
+                        Download Resume
                         <span className='group-hover:rotate-90 duration-300'>
                             <HiArrowNarrowRight className='ml-3 ' />
                         </span>
-                    </button>
+                    </a>
                 </div>
             </div>
         </div>
